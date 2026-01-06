2b9a216ea51f7549d10248b2e2f0663512526823
lywwind <lywwind@gmail.com>
2026-01-06 13:01:20 +0900

    test

--- examples/basic-nextjs-pages-router/.sitecore/component-map.ts
+++ examples/basic-nextjs-pages-router/.sitecore/component-map.ts
@@ -1,18 +1,48 @@
 // Below are built-in components that are available in the app, it's recommended to keep them as is
+
 import { BYOCWrapper, NextjsContentSdkComponent, FEaaSWrapper } from '@sitecore-content-sdk/nextjs';
 import { Form } from '@sitecore-content-sdk/nextjs';
+
 // end of built-in components
-
-// Components imported from the app itself
+import * as Promo from 'src/components/promo/Promo';
 import * as PartialDesignDynamicPlaceholder from 'src/components/partial-design-dynamic-placeholder/PartialDesignDynamicPlaceholder';
+import * as Title from 'src/components/title/Title';
+import * as EventInfo from 'src/components/tac/event-info/EventInfo';
+import * as RowSplitter from 'src/components/row-splitter/RowSplitter';
+import * as RichText from 'src/components/rich-text/RichText';
+import * as PageContent from 'src/components/page-content/PageContent';
+import * as Navigation from 'src/components/navigation/Navigation';
+import * as LinkList from 'src/components/link-list/LinkList';
+import * as Image from 'src/components/image/Image';
+import * as SitecoreStyles from 'src/components/content-sdk/SitecoreStyles';
+import * as CdpPageView from 'src/components/content-sdk/CdpPageView';
+import * as ContentBlock from 'src/components/content-block/ContentBlock';
+import * as Container from 'src/components/container/Container';
+import * as ColumnSplitter from 'src/components/column-splitter/ColumnSplitter';
 
-
-// Components must be registered within the map to match the string key with component name in Sitecore
 export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['BYOCWrapper', BYOCWrapper],
   ['FEaaSWrapper', FEaaSWrapper],
   ['Form', Form],
-  ['PartialDesignDynamicPlaceholder', PartialDesignDynamicPlaceholder],
+  ['Promo', { ...Promo }],
+  ['PartialDesignDynamicPlaceholder', { ...PartialDesignDynamicPlaceholder }],
+  ['EventInfo', { ...EventInfo }],
+  ['Title', { ...Title }],
+  ['EventInfo', { ...EventInfo }],
+  ['RowSplitter', { ...RowSplitter }],
+  ['RichText', { ...RichText }],
+  ['Promo', { ...Promo }],
+  ['PartialDesignDynamicPlaceholder', { ...PartialDesignDynamicPlaceholder }],
+  ['PageContent', { ...PageContent }],
+  ['Navigation', { ...Navigation, componentType: 'client' }],
+  ['LinkList', { ...LinkList }],
+  ['Image', { ...Image }],
+  ['SitecoreStyles', { ...SitecoreStyles, componentType: 'client' }],
+  ['CdpPageView', { ...CdpPageView, componentType: 'client' }],
+  ['ContentBlock', { ...ContentBlock }],
+  ['Container', { ...Container }],
+  ['ColumnSplitter', { ...ColumnSplitter }],
 ]);
 
 export default componentMap;
+
